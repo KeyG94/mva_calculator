1fca1570b66011d96e35fcb005e469ebb54f3e1d
Glenn Key <glennkey78@live.no>
2021-01-03 12:36:33 +0100

    added a new function to calculate mva on alcohol (25%)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let output = document.querySelector('#output');
+let outputAlcohol = document.querySelector('#output-alcohol');
 let product = document.querySelector('#product');
+let alcohol = document.querySelector('#alcohol');
 
 product.addEventListener("keyup", function(){
 
@@ -16,6 +18,20 @@ product.addEventListener("keyup", function(){
     }
 });
 
+alcohol.addEventListener("keyup", function(){
+    if(alcohol.value){
+        setTimeout(() => {
+            findOriginalPriceAlcohol()
+        }, 100)
+    } else {
+        output.innerHTML = `
+        <p>
+        Pris uten mva: 
+        </p>
+        `;
+    }
+})
+
 const findOriginalPrice = () => {
     let productPrice = product.value
     //first: findSum including mva.
@@ -23,6 +39,22 @@ const findOriginalPrice = () => {
     //Two: find mva is divided by 115
     //sum divide by findMva
     const findMva = price / 115;
+    //new sum * 100
+    const originalPrice = findMva * 100;
+    //output
+    output.innerHTML = `
+    <p>Pris uten mva: ${originalPrice} NOK
+    </p>`;
+};   
+
+const findOriginalPriceAlcohol = () => {
+    let productPrice = alcohol.value;
+    let output = outputAlcohol;
+    //first: findSum including mva.
+    const price = parseFloat(productPrice);
+    //Two: find mva is divided by 125
+    //sum divide by findMva
+    const findMva = price / 125;
     //new sum * 100
     const originalPrice = findMva * 100;
     //output
